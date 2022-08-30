0aac82f56b6da4199f5e7b2fa2e46aa9903de0e0
Komal <waghmarekomal21@gmail.com>
2022-08-30 20:18:21 +0530

    [Komal] Factor

---
+++ Factors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class Factors
+    {
+        public void Fact()
+        {
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= num; i++)
+            {
+                while (num % i == 0)
+                {
+                    Console.WriteLine(i + "");
+                    num = num / i;
+                }
+            }
+        }
+    }
+}
+
