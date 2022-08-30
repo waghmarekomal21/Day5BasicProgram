a2c38a9888271e49729b984cf65f30a481d203f9
Komal <waghmarekomal21@gmail.com>
2022-08-30 20:15:24 +0530

    [Komal] Harmonic Number

---
+++ HarmonicNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class HarmonicNumber
+    {
+        int n = 5;
+        double value = 0.0;
+        public void Number()
+        {
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("1/{0} + ", i);
+                value += 1 / (float)i;
+            }
+            Console.Write("\nSum of Series upto {0} terms : {1} \n", n, value);
+        }
+    }
+}
