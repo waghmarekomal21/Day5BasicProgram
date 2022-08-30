0819921e0663a0add53ecaef6e3f7f8b9606f0a0
Komal <waghmarekomal21@gmail.com>
2022-08-30 20:34:53 +0530

    [Komal] Even Or Odd

---
+++ OddEven.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class OddEven
+    {
+        public void Read()
+        {
+            Console.WriteLine("enter any number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine(num + " is even number");
+            }
+            else
+            {
+                Console.WriteLine(num + " is odd number");
+            }
+        }
+    }
+}
